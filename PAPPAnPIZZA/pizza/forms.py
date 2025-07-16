4435cd73750ca4dfcaf0a7db3b453f9d110500e6
suj1thj <s02@tamu.edu>
2025-07-16 12:05:12 +0530

    changed forms format to use data from the Pizza model

--- PAPPAnPIZZA/pizza/forms.py
+++ PAPPAnPIZZA/pizza/forms.py
@@ -1,11 +1,21 @@
 from django import forms
-
-class PizzaForm(forms.Form):
-    Topping1 = forms.CharField(label="Topping1", max_length=100)
-    Topping2 = forms.CharField(label="Topping2", max_length=100)
-    size = forms.ChoiceField(label="Size", choices=[
-        ('small', 'Small'),
-        ('medium', 'Medium'),
-        ('large', 'Large'),
-    ])
+from .models import Pizza, Size
+# class PizzaForm(forms.Form):
+#     Topping1 = forms.CharField(label="Topping1", max_length=100)
+#     Topping2 = forms.CharField(label="Topping2", max_length=100)
+#     size = forms.ChoiceField(label="Size", choices=[
+#         ('small', 'Small'),
+#         ('medium', 'Medium'),
+#         ('large', 'Large'),
+#     ])
     
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = Pizza
+        fields = ['topping1', 'topping2', 'size']
+        labels = {
+            'topping1': 'Topping 1',
+            'topping2': 'Topping 2',
+            'size': 'Size'
+        }
+        
