4eb737ab26506b75eb8d443dd2eda01bc2e04091
suj1thj <s02@tamu.edu>
2025-07-16 12:01:57 +0530

    Added models for size and pizza toppings...and entries for sizes in the admin page manually

--- PAPPAnPIZZA/pizza/views.py
+++ PAPPAnPIZZA/pizza/views.py
@@ -6,7 +6,15 @@ def home(request):
 
 
 def order(request):
-    form= PizzaForm()
+    if request.method == "POST":
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            # Process the order here
+            note="Your order has been placed successfully! your %s %s and %s pizza is on the way" % (filled_form.cleaned_data['size'], filled_form.cleaned_data['Topping1'], filled_form.cleaned_data['Topping2'])
+            new_form = PizzaForm()
+            return render(request, "pizza/order.html", {"pizza_form": new_form, "note": note})
+    else:
+        form = PizzaForm()
     return render(request, "pizza/order.html", {"pizza_form": form})
     # if request.method == "POST":
     #     # Process the order here
